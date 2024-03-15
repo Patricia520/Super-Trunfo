53cb13e3731801fd45c59cf7e3ea6cb36ca28873
patrícia gonçalves ramos <131973939+Patricia520@users.noreply.github.com>
2024-03-15 08:22:58 -0300

    Add files via upload

---
+++ supertrunfo_patriciag/js/script.js
@@ -0,0 +1,299 @@
+var jogador1 = {
+    nome: "Calleri",
+    imagem: "https://uploads.metropoles.com/wp-content/uploads/2022/04/13161455/GettyImages-1347358372.jpg",
+
+    atributos: {
+        velocidade: 32,
+        finalização: 17,
+        passe: 40
+    }
+};
+var jogador2 = {
+    nome: "Kaka",
+    imagem: "https://placar.com.br/wp-content/uploads/2021/10/alx_459751846_original.jpeg",
+
+    atributos: {
+        velocidade: 52,
+        finalização: 47,
+        passe: 78
+    }
+};
+
+var jogador3 = {
+    nome: "Nestor",
+    imagem: "https://static.gazetaesportiva.com/uploads/imagem/2023/11/06/rodrigo-nestor.jpg.webp",
+
+    atributos: {
+        velocidade: 30,
+        finalização: 10,
+        passe: 55
+    }
+};
+var jogador4 = {
+    nome: "Carlos Miguel",
+    imagem: "https://cdn.acritica.net/img/pc/780/530/dn_arquivo/2023/08/1690920559.jpg",
+
+    Atributos: {
+        velocidade: 50,
+        finalização: 50,
+        passe: 95
+    }
+};
+
+var jogador5 = {
+    nome: "wesley",
+    imagem: "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/09/03/gazeta-press-foto-1922205-sof3emdo19v9.jpg",
+
+    atributos: {
+        velocidade: 95,
+        finalização: 90,
+        passe: 90
+    }
+};
+
+var jogador6 = {
+    nome: "Roger guedes",
+    imagem: "https://conteudo.imguol.com.br/c/esporte/74/2023/05/31/roger-guedes-celebra-gol-do-corinthians-sobre-o-atletico-mg-em-jogo-da-copa-do-brasil-1685585701219_v2_900x506.jpg",
+
+    atributos: {
+        velocidade: 80,
+        finalização: 100,
+        passe: 95
+    }
+};
+
+var jogador7 = {
+    nome: "Eduardo Pereira Rodrigues",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9MqQVu2-xE3KCiMQ-uJlsY2hISSxG1X1qZg&usqp=CAU",
+
+    atributos: {
+        velocidade: 93,
+        finalização: 80,
+        passe: 70
+    }
+};
+
+var jogador8 = {
+    nome: "Endrick Felipe",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPHgEr7Xodd_T-9thn1WT2T1hirHAbopnd8Q&usqp=CAU",
+
+    atributos: {
+        velocidade: 95,
+        finalização: 98,
+        passe: 65
+    }
+};
+
+var jogador9 = {
+    nome: "Piquerez",
+    imagem: "https://pubimg.band.uol.com.br/files/4d9c63bd77ee4e49c1ee.webp",
+
+    atributos: {
+        velocidade: 80,
+        finalização: 70,
+        passe: 90
+    }
+};
+
+var jogador10 = {
+    nome: "Vini Jr",
+    imagem: "https://pbs.twimg.com/profile_images/1623036534160035872/5mXtSOFO_400x400.jpg",
+
+    atributos: {
+        velocidade: 70,
+        finalização: 63,
+        passe: 54
+    }
+};
+
+var jogador11 = {
+    nome: "Pique",
+    imagem: "https://s2-quem.glbimg.com/wmli8M2PSqQ61TU_dofEkJWfj7k=/0x0:424x388/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2022/J/C/OIU6RiR6m5UCrSFNBHBA/2020-01-29-gerard-pique.jpeg",
+
+    atributos: {
+        velocidade: 50,
+        finalização: 31,
+        passe: 60
+    }
+};
+
+var jogador12 = {
+    nome: "Ronaldinho Gaúcho",
+    imagem: "https://cartoriogaucho.org.br/wp-content/uploads/2021/07/Ronaldinho-Ga%C3%BAcho.jpg",
+
+    atributos: {
+        velocidade: 80,
+        finalização: 93,
+        passe: 98
+    }
+};
+
+var jogador13 = {
+    nome: "Kylian Mbappé",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjGwlwzwCn4JWC7b741RAu_54gDjjagx_EXg&usqp=CAU",
+
+    atributos: {
+        velocidade: 100,
+        passe: 80,
+        finalização: 85
+    }
+};
+
+var jogador14 = {
+    nome: "Ronaldo Nazário",
+    imagem: "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQG9naorTAS5KAdmTT-BDd6NOya9Iluh_eDxO8qc7l1fN97WVvC9qR-wScntzDFze9M",
+
+    atributos: {
+        velocidade: 97,
+        passe: 90,
+        finalização: 90
+    }
+};
+
+var jogador15 = {
+    nome: "Robert Lewandowski",
+    imagem: "https://a.espncdn.com/photo/2020/1024/r765310_1296x729_16-9.jpg",
+
+    atributos: {
+        velocidade: 90,
+        passe: 85,
+        finalização: 89
+    }
+};
+
+var jogador16 = {
+    nome: "Neymar",
+    imagem: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt7507270b526494f3/638c961f66367a1c91d3f179/NEEEY.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+
+    atributos: {
+        velocidade: 75,
+        finalização: 95,
+        passe: 85
+    }
+};
+
+var jogador17 = {
+    nome: "Rodrygo",
+    imagem: "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/11/53348100119_0722e639f6_k-e1700769401712.jpg?w=1220&h=674&crop=1",
+
+    atributos: {
+        velocidade: 80,
+        finalização: 75,
+        passe: 90
+    }
+};
+
+var jogador18 = {
+    nome: "Pelé",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-PYRD65v7IJfDXbUx7FHVM-PIgWrlb-qKnQ&usqp=CAU",
+
+    atributos: {
+        velocidade: 90,
+        finalização: 90,
+        passe: 90
+    }
+};
+
+var jogador19 = {
+    nome: "Lionel Messi",
+    imagem: "https://phantom-marca.unidadeditorial.es/a62f7db02173f51b940854fcf4a6ea38/crop/156x0/1874x1146/resize/828/f/jpg/assets/multimedia/imagenes/2022/12/28/16722637414809.png",
+
+    atributos: {
+        velocidade: 100,
+        finalização: 95,
+        passe: 95
+    }
+};
+
+var jogador20 = {
+    nome: "Cristiano Ronaldo",
+    imagem: "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/10/cristiano-ronaldo-e1697230106138.jpg?w=1200&h=1200&crop=1",
+
+    atributos: {
+        velocidade: 100,
+        finalização: 95,
+        passe: 95
+    }
+};
+
+
+var jogador21 = {
+    nome: "Gabriel Jesus",
+    imagem: "https://www.lance.com.br/galerias/wp-content/uploads/2020/08/GabrielJesus2-ManchesterCity-x-RealMadrid.jpg",
+
+    atributos: {
+        velocidade: 100,
+        finalização: 95,
+        passe: 95
+    }
+};
+
+var jogador22 = {
+    nome: "Raphael Veiga",
+    imagem: "https://opopular.com.br/image/policy%3A1.3029236%3A1684354365%2Fimage.jpg%3Ff%3D3x2",
+
+    atributos: {
+        velocidade: 80,
+        finalização: 90,
+        passe: 100
+    }
+};
+
+
+var jogador23 = {
+    nome: "Gustavo Scarpa",
+    imagem: " https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltd02eda966850e54a/62b0c777b1b0ef7af057a511/palmeiras_scarpa.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+
+    atributos: {
+        velocidade: 100,
+        finalização: 80,
+        passe: 100
+    }
+};
+
+
+var jogador24 = {
+    nome: "Gustavo Gómez",
+    imagem: "https://s2-ge.glbimg.com/z54UTz1dyPGq0vrSAtPSoJnir6U=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/7/x/diefgHTGAQXh4Wi2AGuA/52152242144-8797776d3c-o.jpg",
+
+    atributos: {
+        velocidade: 90,
+        finalização: 60,
+        passe: 80
+    }
+};
+
+// declaração de um array chamado 'cartas' que armazena objetos 
+var cartas = [ jogador1, jogador2, jogador3, jogador4, jogador5, jogador6, jogador7, jogador8, jogador9, jogador10, jogador11, jogador12,jogador13, jogador14, jogador15, jogador16, jogador17, jogador18, jogador19, jogador20, jogador21, jogador22, jogador23, jogador24];
+
+// função para sortear uam carta aleatoria do array cartas
+function sortearCarta(){
+    // gera um indice aleatorio para escolher uma carta do array
+    var cartaSorteada = cartas[Math.floor(Math.random() * cartas.length)];
+    // chama a função para exibir a carta sorteada
+    exibirCarta(cartaSorteada);
+}
+// funçao exibir os detalhes da carta na interface
+function exibirCarta(carta){
+    var cartaJogadorElement= document.getElementById('carta-jogador');
+    cartaJogadorElement.querySelector('img').src = carta.imagem;
+    cartaJogadorElement.querySelector('h3').textContent = carta.nome;
+    // obter os atributos da carta
+    var atributos = carta.atributos;
+    // Criar uma string HTML com os atributos corretos
+    var atributosHTML = `<p>velocidade: ${atributos.velocidade}</p> 
+    <p>finalização:${atributos.finalização}</p> <p>passe:${atributos.passe}</p>`;
+    // Atualiza o conteudo da DIv .carta-atributos
+    cartaJogadorElement.querySelector('.carta-atributos').innerHTML = atributosHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
